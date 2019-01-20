01b357ce9e3e96cc108e54971c99a2d51117f178
NnN <nihal.y.baig@gmail.com>
2019-01-20 07:23:10 +0530

    icon

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -9,8 +9,6 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 //import AppBar from 'material-ui/AppBar';
 
-//import CommonCart from './CommonCart.jsx';
-
 class News extends Component {
   render() {
     return (
@@ -20,11 +18,6 @@ class News extends Component {
             {/* <div className="home-pararaph">
                 This is About page
                 
-            <CommonCart/>
-                <p>You’ll need a React web app to add react-router.If you need to create one, the easiest way 
-                    to get started is with a popular tool called Create React App.First install create-react-app, 
-                    if you don’t already have it, and then make a new project with it.</p>
-            </div>
             <Link to="/">
                 <button> CLick Me to go to home </button>
             </Link> */}
