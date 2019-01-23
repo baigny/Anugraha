6e7eee00971d25c1aeac22717e1311815dd9b0ad
baigny <baigny@gmail.com>
2019-01-24 00:43:42 +0530

    tux : changed button position

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,27 +1,14 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import {Jumbotron, Grid, Button} from 'react-bootstrap';
+import React from 'react';
+import '../styles/gallery.css';
 
-import '../styles/home.css'
-
-
-
-export default class Gallery extends Component {
-  render() {
-    return (
-      <div> 
-         <Grid>
-            <Jumbotron>
-                <h2> Welcome to Anugraha Women's College</h2>
-                <p>GAllery</p>
-            </Jumbotron>    
-          </Grid>
-          <Link to="/about">
-               <Button bsStyle="primary" >About</Button>
-          </Link>
-      </div>
-    );
-  }
+export default class Gallery extends React.Component {
+    render() {
+	return (
+	  <div>
+          
+      </div>  
+	);
+    }
 }
 
 
