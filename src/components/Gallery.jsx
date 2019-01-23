453be618636bcb071113557a1cd82b27f4d676b5
baigny <baigny@gmail.com>
2019-01-24 01:03:24 +0530

    tux : added gallery

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,14 +1,37 @@
 import React from 'react';
-import '../styles/gallery.css';
-
+import ImageGallery from 'react-image-gallery';
+ 
 export default class Gallery extends React.Component {
-    render() {
-	return (
-	  <div>
-          
-      </div>  
-	);
-    }
+ 
+  render() {
+ 
+    const images = [
+      {
+        original: "http://lorempixel.com/1000/600/nature/1/",
+        thumbnail: "http://lorempixel.com/250/150/nature/1/",
+      },
+      {
+        original: "http://lorempixel.com/1000/600/nature/2/",
+        thumbnail: "http://lorempixel.com/250/150/nature/2/"
+      },
+      {
+        original: "http://lorempixel.com/1000/600/nature/3/",
+        thumbnail: "http://lorempixel.com/250/150/nature/3/"
+      }
+      ,
+      {
+        original: "http://lorempixel.com/1000/600/nature/4/",
+        thumbnail: "http://lorempixel.com/250/150/nature/4/"
+      },
+      {
+        original: "http://lorempixel.com/1000/600/nature/5/",
+        thumbnail: "http://lorempixel.com/250/150/nature/5/"
+      }
+    ]
+ 
+    return (
+      <ImageGallery items={images} />
+    );
+  }
+ 
 }
-
-
