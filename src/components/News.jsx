646360476c96e633a9aa72f70d767433563aa540
NnN <nihal.y.baig@gmail.com>
2019-01-24 22:58:59 +0530

    NNN : news

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,31 +1,142 @@
 import React, { Component } from 'react';
-//import { Link } from 'react-router-dom';
-import {redA400} from 'material-ui/styles/colors';
-
+import {  MDBRow, MDBCol, MDBIcon ,MDBContainer} from "mdbreact";
 import '../styles/about.css'
-import HorizontalLinearStepper from './HorizontalLinearStepper.jsx'
-//import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import getMuiTheme from 'material-ui/styles/getMuiTheme';
-//import AppBar from 'material-ui/AppBar';
+
 
 class News extends Component {
   render() {
     return (
-        <MuiThemeProvider muiTheme={getMuiTheme({palette: {
-            textColor: redA400,
-          },})}>
-            {/* <div className="home-pararaph">
-                This is About page
+            <section className="my-5">
+                <MDBContainer className="mb">
+                    <MDBRow>
+                        <MDBCol>
+                         <h2 className="font-weight-bold text-center my-5">
+                                   News and Events
+                         </h2>
+                        </MDBCol>
+                    </MDBRow>
+                </MDBContainer>
                 
-            <Link to="/">
-                <button> CLick Me to go to home </button>
-            </Link> */}
-            <HorizontalLinearStepper/>
-        </MuiThemeProvider>
+                <MDBContainer className="mb">          
+                    <MDBRow >
+                         <MDBCol  md="6"className="mb-lg-0 mb-5">
+                                <img
+                                src="https://mdbootstrap.com/img/Photos/Others/images/83.jpg"
+                                alt=""
+                                className="img-fluid rounded z-depth-1"
+                                />
+                         </MDBCol>
+                                          
+                         <MDBCol  md="6"className="mb-lg-0 mb-5" >
+                             <MDBRow className="mb-3">
+                                <MDBCol md="1" size="2">
+                                    <MDBIcon icon="bar-chart" size="2x" className="indigo-text" />
+                                </MDBCol>
+                                <MDBCol md="11" size="10">
+                                    <h5 className="font-weight-bold mb-3">Marketing</h5>
+                                    <p className="grey-text">
+                                        Considering the educational problems of rural areas of Bantwal taluk, particularly minority muslim girls, 
+                                        in 2011-12 the trust has commenced Women’s PU College 
+                                        at Golthamajal, Kalladka – a central place to serve many nearby localities viz: Kalladka, Panemangalore, Bantwal, BC Road, Farangipete, 
+                                        Manchi, Salettoor, Vittla, Kabaka, Mani etc. 
+                                    </p>
+                                    <p className="grey-text">
+                                        Considering the educational problems of rural areas of Bantwal taluk, particularly minority muslim girls, 
+                                        in 2011-12 the trust has commenced Women’s PU College 
+                                        at Golthamajal, Kalladka – a central place to serve many nearby localities viz: Kalladka, Panemangalore, Bantwal, BC Road, Farangipete, 
+                                        Manchi, Salettoor, Vittla, Kabaka, Mani etc. 
+                                    </p>
+                                    <p className="grey-text">
+                                        Considering the educational problems of rural areas of Bantwal taluk, particularly minority muslim girls, 
+                                        in 2011-12 the trust has commenced Women’s PU College 
+                                        at Golthamajal, Kalladka – a central place to serve many nearby localities viz: Kalladka, Panemangalore, Bantwal, BC Road, Farangipete, 
+                                        Manchi, Salettoor, Vittla, Kabaka, Mani etc. 
+                                    </p>
+                                </MDBCol>
+                             </MDBRow>
+                         </MDBCol>
+                    </MDBRow>
+                </MDBContainer>
+
+                
+                <MDBContainer className="mb">          
+                    <MDBRow >
+                         <MDBCol  md="6"className="mb-lg-0 mb-5">
+                                <img
+                                src="https://mdbootstrap.com/img/Photos/Others/images/82.jpg"
+                                alt=""
+                                className="img-fluid rounded z-depth-1"
+                                />
+                         </MDBCol>
+                                          
+                         <MDBCol  md="6"className="mb-lg-0 mb-5" >
+                             <MDBRow className="mb-3">
+                                <MDBCol md="1" size="2">
+                                    <MDBIcon icon="book" size="2x" className="indigo-text" />
+                                </MDBCol>
+                                <MDBCol md="11" size="10">
+                                    <h5 className="font-weight-bold mb-3">Marketing</h5>
+                                    <p className="grey-text">
+                                        Considering the educational problems of rural areas of Bantwal taluk, particularly minority muslim girls, 
+                                        in 2011-12 the trust has commenced Women’s PU College 
+                                        at Golthamajal, Kalladka – a central place to serve many nearby localities viz: Kalladka, Panemangalore, Bantwal, BC Road, Farangipete, 
+                                        Manchi, Salettoor, Vi
+                                        Considering the educational problems of rural areas of Bantwal taluk, particularly minority muslim girls, 
+                                        in 2011-12 the trust has commenced Women’s PU College 
+                                        at Golthamajal, Kalladka – a central place to serve many nearby localities viz: Kalladka, Panemangalore, Bantwal, BC Road, Farangipete, 
+                                        Manchi, Salettoor, Vittla, Kabaka, Mani etc. 
+                                    </p>
+                                    <p className="grey-text">
+                                        Considering the educational problems of rural areas of Bantwal taluk, particularly minority muslim girls, 
+                                        in 2011-12 the trust has commenced Women’s PU College 
+                                        at Golthamajal, Kalladka – a central place to serve many nearby localities viz: Kalladka, Panemangalore, Bantwal, BC Road, Farangipete, 
+                                        Manchi, Salettoor, Vittla, Kabaka, Mani etc. 
+                                    </p>
+                                </MDBCol>
+                             </MDBRow>
+                         </MDBCol>
+                    </MDBRow>
+                </MDBContainer>
+
+
+                
+                <MDBContainer className="mb">          
+                    <MDBRow >
+                         <MDBCol  md="6"className="mb-lg-0 mb-5">
+                                <img
+                                src="https://mdbootstrap.com/img/Photos/Others/images/83.jpg"
+                                alt=""
+                                className="img-fluid rounded z-depth-1"
+                                />
+                         </MDBCol>
+                                          
+                         <MDBCol  md="6"className="mb-lg-0 mb-5" >
+                             <MDBRow className="mb-3">
+                                <MDBCol md="1" size="2">
+                                    <MDBIcon icon="code" size="2x" className="red-text" />
+                                </MDBCol>
+                                <MDBCol md="11" size="10">
+                                    <h5 className="font-weight-bold mb-3">Marketing</h5>
+                                    <p className="grey-text">
+                                        Considering the educational problems of rural areas of Bantwal taluk, particularly minority muslim girls, 
+                                        in 2011-12 the trust has commenced Women’s PU College 
+                                        at Golthamajal, Kalladka – a central place to serve many nearby localities viz: Kalladka, Panemangalore, Bantwal, BC Road, Farangipete, 
+                                        Manchi, Salettoor, Vittla, Kabaka, Mani etc. 
+                                    </p>
+                                    <p className="grey-text">
+                                        Considering the educational problems of rural areas of Bantwal taluk, particularly minority muslim girls, 
+                                        Manchi, Salettoor, Vittla, Kabaka, Mani etc. 
+                                    </p>
+                                </MDBCol>
+                             </MDBRow>
+                         </MDBCol>
+                    </MDBRow>
+                </MDBContainer>
+
+
+            </section>
     );
   }
 }
 
 export default News;
-  
